cd5d682f0619677c7a1a51140e7b243b55482e62
liuycy <liuycy@yeah.net>
2020-06-18 15:40:20 +0800

    chore: add eslint,prettier,git-hook

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,46 @@
 import { getCurrentInstance, onMounted, onUnmounted } from 'vue';
 
+interface ChromeEvent extends Event {
+    path: EventTarget[];
+}
+
 export default function onClickOutside(
-  hook: (e?: Event) => any,
-  target = getCurrentInstance()
-) {
-  function handleEvent(e: Event) {
-    // not support IE now
-    const path: EventTarget[] = (e as any).path ?? e.composedPath();
+    hook: (e?: Event) => void,
+    target = getCurrentInstance()
+): void {
+    function handleEvent(e: Event) {
+        if (!target) return;
 
-    const nodes: any[] = [];
+        // not support IE now
+        const path: EventTarget[] = (e as ChromeEvent).path ?? e.composedPath();
 
-    if (typeof target!.subTree.type === 'symbol') {
-      // Fragment
-      Array.prototype.map.call(target!.subTree.children, ({ el }) => {
-        nodes.push(el);
-      });
-    } else {
-      // Single root
-      nodes.push(target!.subTree.el);
+        const nodes: EventTarget[] = [];
+
+        if (typeof target.subTree.type === 'symbol') {
+            // Fragment
+            Array.prototype.map.call(target.subTree.children, ({ el }) => {
+                nodes.push(el);
+            });
+        } else {
+            // Single root
+            nodes.push(target.subTree.el as EventTarget);
+        }
+
+        const isNotClickOutside = nodes.some((el) => ~path.indexOf(el));
+
+        if (isNotClickOutside) return;
+
+        hook.call(null, e);
     }
 
-    const isNotClickOutside = nodes.some((el) => ~path.indexOf(el));
+    onMounted(() => {
+        if (!target?.subTree.children) return;
+        window.addEventListener('click', handleEvent);
+        window.addEventListener('touchend', handleEvent);
+    });
 
-    if (isNotClickOutside) return;
-
-    hook.call(null, e);
-  }
-
-  onMounted(() => {
-    if (!target) return;
-    if (!target.subTree.children) return;
-    window.addEventListener('click', handleEvent);
-    window.addEventListener('touchend', handleEvent);
-  });
-
-  onUnmounted(() => {
-    window.removeEventListener('click', handleEvent);
-    window.removeEventListener('touchend', handleEvent);
-  });
+    onUnmounted(() => {
+        window.removeEventListener('click', handleEvent);
+        window.removeEventListener('touchend', handleEvent);
+    });
 }
